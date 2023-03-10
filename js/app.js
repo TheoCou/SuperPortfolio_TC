33e78197b96b114063cd939a48458342ba77b2c5
TheoCou <91134816+TheoCou@users.noreply.github.com>
2023-03-10 16:04:20 +0100

    deleted:    asset/RESPONSIVE_1.png 	new file:   asset/fidelio.png 	deleted:    asset/pipo.jpg 	new file:   asset/streaming.png 	new file:   asset/ventepc.png 	modified:   css/main.css 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", e =>{
+document.addEventListener("DOMContentLoaded", e => {
     window.addEventListener("scroll", function () {
         let header = document.querySelector("header");
         header.classList.toggle("sticky", window.scrollY > 0);
     })
 
-    window.addEventListener("scroll", function (){
+    window.addEventListener("scroll", function () {
         let navbar_mobile = document.querySelector("nav");
         navbar_mobile.classList.toggle("sticky", window.scrollY > 0);
     })
